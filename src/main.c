48cc7a3b0f73e70eff71fa0d7caed3babb51516d
Kristian Lyngstol <kristian@nihilus.kristian.int>
2009-11-26 22:46:26 +0100

    Initial commit. Hi.

---
+++ src/main.c
@@ -0,0 +1,27 @@
+/* Kristian's Window Management Daemon
+ * Copyright (C) 2009 Kristian Lyngstøl <kristian@bohemians.org>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <stdio.h>
+#include <X11/Xlib.h>
+#include "core.h"
+core kwmd;
+
+int main(int argc, char **argv) {
+	kwmd.dpy = XOpenDisplay(NULL);
+	return 0;
+}
